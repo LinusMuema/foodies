af24600abfecfffe355642375ba963d892202be3
linus muema <linus.m.muema@gmail.com>
2020-06-14 11:49:51 +0300

    - code cleanup

--- app/src/main/java/com/moose/foodies/features/BaseActivity.kt
+++ app/src/main/java/com/moose/foodies/features/BaseActivity.kt
@@ -2,21 +2,27 @@ package com.moose.foodies.features
 
 import android.app.Activity
 import android.content.Intent
+import android.content.SharedPreferences
 import android.graphics.Rect
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.moose.foodies.R
 import com.moose.foodies.features.auth.AuthActivity
 import com.moose.foodies.features.favorites.FavoritesActivity
+import com.moose.foodies.features.home.HomeActivity
 import com.nightonke.boommenu.BoomButtons.ButtonPlaceEnum
 import com.nightonke.boommenu.BoomButtons.SimpleCircleButton
 import com.nightonke.boommenu.Piece.PiecePlaceEnum
 import kotlinx.android.synthetic.main.activity_base.*
+import javax.inject.Inject
 
-open class BaseActivity: AppCompatActivity() {
+open class BaseActivity @Inject constructor(): AppCompatActivity() {
+
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
 
     fun setUpBoomMenu() {
-        val icons = arrayOf(R.drawable.ic_logout, R.drawable.ic_favorites, R.drawable.ic_account, R.drawable.ic_baseline_info_24, R.drawable.ic_search)
+        val icons = arrayOf(R.drawable.ic_home, R.drawable.ic_favorites, R.drawable.ic_search, R.drawable.ic_account, R.drawable.ic_logout)
         for (icon in icons){
             val builder = SimpleCircleButton.Builder()
                 .normalColorRes(R.color.primary)
@@ -33,21 +39,26 @@ open class BaseActivity: AppCompatActivity() {
     }
 
     private fun handleBoomClick(index: Int) {
+        val activity = this.localClassName
+        Log.d("Activity", "handleBoomClick: ${this.localClassName}")
         when(index){
             0 -> {
-                startActivity(Intent(this, AuthActivity::class.java))
+                if (!activity.contains("HomeActivity"))
+                    startActivity(Intent(this, HomeActivity::class.java))
             }
             1 -> {
-                startActivity(Intent(this, FavoritesActivity::class.java))
+                if (!activity.contains("FavoritesActivity"))
+                    startActivity(Intent(this, FavoritesActivity::class.java))
             }
             2 -> {
-                Log.d("Boom", "handleBoomClick: accounts clicked")
+                Log.d("Boom", "handleBoomClick: search clicked")
             }
             3 -> {
-                Log.d("Boom", "handleBoomClick: info clicked")
+                Log.d("Boom", "handleBoomClick: account clicked")
             }
             4 -> {
-                Log.d("Boom", "handleBoomClick: search clicked")
+                sharedPreferences.edit().putBoolean("logged", false).apply()
+                startActivity(Intent(this, AuthActivity::class.java))
             }
         }
     }
