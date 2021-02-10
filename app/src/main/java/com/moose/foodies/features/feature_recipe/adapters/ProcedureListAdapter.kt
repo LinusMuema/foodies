705ae3fbb8eaf154369997b748d38ae3015f5439
linus muema <linus.m.muema@gmail.com>
2021-02-10 22:31:31 +0300

    - clean the extensions file

--- app/src/main/java/com/moose/foodies/features/feature_recipe/adapters/ProcedureListAdapter.kt
+++ app/src/main/java/com/moose/foodies/features/feature_recipe/adapters/ProcedureListAdapter.kt
@@ -1,39 +1,37 @@
 package com.moose.foodies.features.feature_recipe.adapters
 
-import android.view.LayoutInflater
+import android.view.LayoutInflater.from
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.moose.foodies.databinding.ProcedureListItemBinding
+import com.moose.foodies.databinding.ProcedureListItemBinding.inflate
 import com.moose.foodies.features.feature_home.domain.Section
 import com.moose.foodies.features.feature_recipe.adapters.ProcedureListAdapter.ProcedureViewHolder
 import com.moose.foodies.util.hide
 
-class ProcedureListAdapter(private val procedures: List<Section>): Adapter<ProcedureViewHolder>() {
+class ProcedureListAdapter(private val sections: List<Section>): Adapter<ProcedureViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProcedureViewHolder {
-        val binding = ProcedureListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = inflate(from(parent.context), parent, false)
         return ProcedureViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ProcedureViewHolder, position: Int) {
-        holder.bind(procedures[position])
-    }
+        val section = sections[position]
+        with(holder.binding){
+            if (section.name == "") procedureName.hide()
+            else procedureName.text = section.name
 
-
-    override fun getItemCount(): Int = procedures.size
-
-    inner class ProcedureViewHolder(private val binding: ProcedureListItemBinding): ViewHolder(binding.root) {
-
-        fun bind(section: Section) {
-            if (section.name == "") binding.procedureName.hide()
-            else binding.procedureName.text = section.name
-
-            binding.stepRecycler.apply {
+            stepRecycler.apply {
                 setHasFixedSize(true)
                 adapter = StepListAdapter(section.steps)
             }
         }
-
     }
+
+
+    override fun getItemCount(): Int = sections.size
+
+    class ProcedureViewHolder(val binding: ProcedureListItemBinding): ViewHolder(binding.root)
 }
--- app/src/main/java/com/moose/foodies/util/Extensions.kt
+++ app/src/main/java/com/moose/foodies/util/Extensions.kt
@@ -6,22 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import android.view.inputmethod.InputMethodManager
-import android.widget.ImageView
 import android.widget.Toast
-import androidx.core.content.ContextCompat
-import androidx.fragment.app.Fragment
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.bumptech.glide.request.RequestOptions
-import com.google.android.material.snackbar.Snackbar
 import com.moose.foodies.R
 import com.moose.foodies.features.feature_home.domain.Item
 
 fun View.fadeIn() = this.startAnimation(AnimationUtils.loadAnimation(this.context, R.anim.fade_in))
 
 fun View.slideUp() = this.startAnimation(AnimationUtils.loadAnimation(this.context, R.anim.slide_up))
-
-fun View.show() { this.visibility = View.VISIBLE }
 
 fun View.hide() { this.visibility = View.GONE }
 
@@ -65,38 +56,7 @@ fun Activity.hideKeyPad(): Boolean {
         .hideSoftInputFromWindow((currentFocus ?: View(this)).windowToken, 0)
 }
 
-fun Fragment.hideKeyboard(): Boolean {
-    return (context?.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager)
-        .hideSoftInputFromWindow((activity?.currentFocus ?: View(context)).windowToken, 0)
-}
-
-
-fun ImageView.loadRoundImage(uri: String){
-    val options = RequestOptions()
-        .circleCrop()
-        .error(R.drawable.image_error)
-
-    Glide.with(this.context).setDefaultRequestOptions(options).load(uri).into(this)
-}
-
-fun showSnackbar(view: View, message: String) = Snackbar.make(view, message, Snackbar.LENGTH_LONG).show()
-
-fun ImageView.loadCarouselImage(uri: String){
-    val options = RequestOptions()
-        .transform(RoundedCorners(25))
-        .error(R.drawable.image_error)
-
-    Glide.with(this.context).setDefaultRequestOptions(options).load(uri).into(this)
-}
-
 fun Context.showToast(message: String?){
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-}
-
-fun ImageView.loadDrawable(drawable: Int) = Glide.with(this.context).load(ContextCompat.getDrawable(this.context, drawable)).into(this)
-
-fun ImageView.loadImage(uri: String) {
-    val options = RequestOptions().error(R.drawable.image_error)
-    Glide.with(this.context).setDefaultRequestOptions(options).load(uri).into(this)
 }
 
