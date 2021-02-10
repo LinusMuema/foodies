44185fa18898c68e238f7b2732b60283534043d1
linus muema <linus.m.muema@gmail.com>
2021-02-10 22:52:16 +0300

    - round value to 1dp

--- app/src/main/java/com/moose/foodies/features/feature_ingredients/adapters/MissedIngredientsAdapter.kt
+++ app/src/main/java/com/moose/foodies/features/feature_ingredients/adapters/MissedIngredientsAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import coil.load
+import com.moose.foodies.R
 import com.moose.foodies.databinding.MissingIngredientBinding
 import com.moose.foodies.databinding.MissingIngredientBinding.inflate
 import com.moose.foodies.features.feature_ingredients.adapters.MissedIngredientsAdapter.MissedIngredientsViewHolder
@@ -23,10 +24,13 @@ class MissedIngredientsAdapter(
 
     override fun onBindViewHolder(holder: MissedIngredientsViewHolder, position: Int) {
         val ingredient = ingredients[position]
+        val value = String.format("%.1f", ingredient.amount)
+        val context = holder.binding.root.context
+
         with(holder.binding){
             image.load(ingredient.image)
             name.text = ingredient.name
-            amount.text = "${ingredient.amount} ${ingredient.unitShort}"
+            amount.text = context.getString(R.string.amount, value, ingredient.amount)
         }
     }
 
