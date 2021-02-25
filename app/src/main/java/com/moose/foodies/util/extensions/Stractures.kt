345507e3f5b8dfe75435445612dcb244d563524d
linus muema <linus.m.muema@gmail.com>
2021-02-25 20:06:53 +0300

    - remove unused extension functions

--- app/src/main/java/com/moose/foodies/util/extensions/Stractures.kt
+++ app/src/main/java/com/moose/foodies/util/extensions/Stractures.kt
@@ -6,6 +6,4 @@ fun String.largeImage(): String = this.replace("312x231", "636x393")
 
 fun String.mediumImage(): String = this.replace("312x231", "240x150")
 
-fun String.smallImage(): String = this.replace("312x231", "90x90")
-
 fun List<Item>.clean(): List<Item> = this.toSet().toList()
