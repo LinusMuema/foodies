099b33ed7f2e716e4ac53189c0c2a727d714972c
linus muema <linus.m.muema@gmail.com>
2021-09-15 11:03:29 +0300

    - fixed the theme colors

--- app/src/main/java/com/moose/foodies/features/splash/SplashActivity.kt
+++ app/src/main/java/com/moose/foodies/features/splash/SplashActivity.kt
@@ -3,6 +3,9 @@ package com.moose.foodies.features.splash
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -10,40 +13,32 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.moose.foodies.theme.FoodiesTheme
 
+@ExperimentalAnimationApi
 class SplashActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FoodiesTheme {
-
-                // set the system bar colors
-                val systemUiController = rememberSystemUiController()
-                systemUiController.setSystemBarsColor(color = MaterialTheme.colors.primary)
-
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colors.primary) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        val visible by remember { mutableStateOf(true) }
+                        AnimatedVisibility(visible = visible, enter = fadeIn()) {
+                            Text("Foodies", style = MaterialTheme.typography.h5)
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    FoodiesTheme {
-        Greeting("Android")
     }
 }
