9f1625c756974fcfccc1a2c2fac760b5ac010be3
linus muema <linus.m.muema@gmail.com>
2021-09-16 09:25:52 +0300

    - added the viewmodels

---
+++ app/src/main/java/com/moose/foodies/features/auth/AuthViewmodel.kt
@@ -0,0 +1,8 @@
+package com.moose.foodies.features.auth
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class AuthViewmodel: ViewModel() {
+}
---
+++ app/src/main/java/com/moose/foodies/features/splash/SplashViewmodel.kt
@@ -0,0 +1,8 @@
+package com.moose.foodies.features.splash
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class SplashViewmodel: ViewModel() {
+}
